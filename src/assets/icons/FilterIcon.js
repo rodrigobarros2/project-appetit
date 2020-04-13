cacd4cb63830cf0905ea0626b437c44d87731ac4
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-04-13 11:44:21 -0300

    Componetizando icones do projeto

---
+++ src/assets/icons/FilterIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function FilterIcon() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" className="icon-svg" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path fill-rule="evenodd" clip-rule="evenodd" d="M14 17H3.99558C3.44484 17 3 16.5523 3 16C3 15.4439 3.44574 15 3.99558 15H14V17ZM20.001 17H18V15H20.001C20.5573 15 21 15.4477 21 16C21 16.5561 20.5528 17 20.001 17ZM3 8C3 7.44386 3.44725 7 3.99896 7H6V9H3.99896C3.44266 9 3 8.55228 3 8ZM10 9V7H20.0044C20.5552 7 21 7.44772 21 8C21 8.55614 20.5543 9 20.0044 9H10Z" className="icon-fill"/>
+			<path fill-rule="evenodd" clip-rule="evenodd" d="M16 19C14.3431 19 13 17.6569 13 16C13 14.3431 14.3431 13 16 13C17.6569 13 19 14.3431 19 16C19 17.6569 17.6569 19 16 19ZM16 17C16.5523 17 17 16.5523 17 16C17 15.4477 16.5523 15 16 15C15.4477 15 15 15.4477 15 16C15 16.5523 15.4477 17 16 17Z" className="icon-fill"/>
+			<path fill-rule="evenodd" clip-rule="evenodd" d="M8 11C6.34315 11 5 9.65685 5 8C5 6.34315 6.34315 5 8 5C9.65685 5 11 6.34315 11 8C11 9.65685 9.65685 11 8 11ZM8 9C8.55228 9 9 8.55228 9 8C9 7.44772 8.55228 7 8 7C7.44772 7 7 7.44772 7 8C7 8.55228 7.44772 9 8 9Z" className="icon-fill"/>
+		</svg>
+    )
+}
---
+++ src/assets/icons/menuCliente.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MenuCliente() {
+    return (
+        <svg width="24" height="24" className="icon-svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 3C14.7614 3 17 5.23858 17 8C17 10.7614 14.7614 13 12 13C9.23858 13 7 10.7614 7 8C7 6.52057 7.64253 5.19121 8.66377 4.27575M3 21C3 18.2386 7.02944 16 12 16C16.9706 16 21 18.2386 21 21" className="icon-stroke" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M9.17065 9H14.8292C14.4174 10.1652 13.3062 11 11.9999 11C10.6937 11 9.58249 10.1652 9.17065 9Z" className="icon-fill" />
+        </svg>
+    )
+}
+
