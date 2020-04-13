a6f1659852d15f0bce2d2255c537bbf56287e2af
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-04-13 11:47:59 -0300

    Colocando todas as rotas da aplicação

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import store from './store/'
 import { Provider } from 'react-redux'
 
+import Login from './pages/Login';
+import Home from './pages/Home';
+import UserOrder from './pages/UserOrder';
+import Orders from './pages/Orders';
+import Details from './pages/Details'
+import Buyer from './pages/Buyer'
+import PaymentStatus from './pages/PaymentStatus'
+import Finished from './pages/Finished'
+
+
 export default function Routes() {
     return (
         <Provider store={store}>
@@ -10,6 +20,12 @@ export default function Routes() {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Login} />
+                    <Route exact path="/userorders" component={UserOrder} />
+                    <Route exact path="/pedido" component={Orders} />
+                    <Route exact path="/detalhes" component={Details} />
+                    <Route exact path="/comprador" component={Buyer} />
+                    <Route exact path="/statuspagamento" component={PaymentStatus} />
+                    <Route exact path="/finalizado" component={Finished} />
                 </Switch>
             </BrowserRouter>
         </Provider>
