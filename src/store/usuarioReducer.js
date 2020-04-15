99ba772c049c83951f671753c68c43785712c09d
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-04-15 00:33:28 -0300

    Reorganizando o redux

--- src/store/usuarioReducer.js
+++ src/store/usuarioReducer.js
@@ -1,17 +1,53 @@
 const INITIAL_STATE = {
     usuarioEmail: '',
     usuarioLogado: '0',
+    cart: {
+        items: [],
+        total: 0
+    }
 };
 
-function usuarioReducer(state = INITIAL_STATE, action) {
-    switch (action.type) {
-        case 'LOG_IN':
-            return { ...state, usuarioLogado: 1, usuarioEmail: action.usuarioEmail }
-        case 'LOG_OUT':
-            return { ...state, usuarioLogado: 0, usuarioEmail: null }
-        default:
-            return state;
-    }
+const actionType = {
+    LOG_IN(state, action) {
+        return {
+            ...state,
+            usuarioLogado: 1,
+            usuarioEmail: action.usuarioEmail
+        };
+    },
+    LOG_OUT(state, action) {
+        return {
+            ...state,
+            usuarioLogado: 0,
+            usuarioEmail: null
+        };
+    },
+    ADD_CART(state, action) {
+        state.cart.items.push({
+            name: action.product.name,
+            quantity: action.product.quantity,
+            option: action.product.option,
+            price: action.product.price
+        });
+
+        state.cart.total = state.cart.items.reduce((prev, item) => {
+            return prev + (item.price * item.quantity);
+        }, 0);
+
+        return {
+            ...state,
+            cart: {
+                ...state.cart,
+                items: [...state.cart.items]
+            }
+        }
+    },
+};
+
+function reducer(state = INITIAL_STATE, action) {
+    const doAction = actionType[action.type];
+
+    return doAction ? doAction(state, action) : state;
 }
 
-export default usuarioReducer;
+export default reducer;
